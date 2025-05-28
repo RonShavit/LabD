6a0b33133a89a52c95d44ccd9a6112c70a0eb9f5
ronshav <ron.sh.10.01@gmail.com>
2025-05-28 20:12:17 +0300

    :(

--- multi.s
+++ multi.s
@@ -2,15 +2,26 @@ section .rodata
     multi_print_fmt db '%02hhx',10,0 ; must add linefeed duo to flushing issues
     number_debug_format db 'only: %X',10,0
     dnumber_format db '1st: %d | 2nd: %d',10,0
+    mask dw 1101001000101101b
+    multi_print_format db 'multi %c:',10,'_______',10,0
+    seperator_foramt db "enter input for multi number %c",10,0
+
+
+
 
 section .data
+    state dw 0xa5
     temp_struct dd 0
+    in_struct dd 0
+    in_struct_2 dd 0
     temp_buffer dd 0
     x_struct: dw 6
     x_number: db 0x70, 0x71, 0x72, 0x73, 0x74, 0x75,
-    y_struct: dw 4
-    y_number: db 100, 0xff, 9, 12
+    y_struct: dw 2
+    y_number: db 0xff, 1
     first_current dw 0
+    temp_num dw 0
+    mode db 0 ; 0=>use x|y, 1=>call get_multi, 2=>call PRmulti
     
     
 
@@ -22,6 +33,119 @@ section .text
     extern free
     extern stdin
     extern fgets
+
+    PRmulti:
+    push ebp
+    mov ebp, esp
+
+    .gen_len:
+        call rand_num
+        xor ah, ah
+        cmp al, 0
+        jz .gen_len
+    ;;ax now contains a non-zero pr-gen num
+    add ax, 2
+    mov bx, ax
+    pushad
+
+    push eax
+    call malloc
+    add esp, 4
+    mov [temp_struct], eax
+    popad
+    sub bx, 2
+    mov esi, [temp_struct]
+    
+    mov [esi], bx
+
+    add esi, 2
+
+    .add_num_loop:
+        cmp bx, 0
+        jz .finish
+        pushad
+        call rand_num
+        mov [temp_num], ax
+        popad
+        mov ax, [temp_num]
+        cmp ax, 0
+        jz .add_num_loop
+
+        mov [esi], al
+        add esi, 1
+        dec bx
+        jmp .add_num_loop
+
+        
+    
+    .finish:
+    mov eax, [temp_struct]
+
+
+    pop ebp
+    ret
+
+    rand_num:
+    push ebp
+    mov ebp, esp
+
+    xor edx, edx
+
+    xor ebx, ebx
+    xor eax, eax
+    mov bx, [state]
+    mov ax, [mask]
+    and ax, bx
+    jpe .par_is_zero
+    mov dx, 1
+    shl dx, 15
+    .par_is_zero:
+    mov bx, [state]
+    shr bx, 1
+    or bx, dx
+    mov [state], bx
+    
+    xor eax, eax
+    mov ax, bx
+
+
+    .finish:
+    pop ebp
+    ret
+
+
+    get_min_max:
+    push ebp
+    mov ebp, esp
+
+    xor eax, eax
+    xor ebx, ebx
+    xor ecx, ecx
+    xor edx, edx
+    
+    mov eax, [esp+8] ;; eax = first_multi
+    mov cx, [eax]  ;; cx = first_multi.len
+
+
+    mov ebx, [esp+12]    ;;ebx = sec_multi
+    mov dx, [ebx]   ;;dx = sec_multi.len
+
+    cmp cx, dx
+    jge .first_larger
+
+    xor esi, esi
+    mov esi, eax
+    mov eax, ebx
+    mov ebx, esi
+
+    .first_larger:
+
+
+    
+    pop ebp
+    ret
+
+
 
     add_multi:
     push ebp
@@ -104,15 +228,10 @@ section .text
     add dx, cx
     mov [esi], dl
 
-    pushad
+    
     xor edx, edx
     mov dl, [esi]
 
-    push edx
-    push number_debug_format
-    call printf
-    add esp, 8
-    popad
 
     inc esi
     inc edi
@@ -329,21 +448,193 @@ section .text
         pop ebp
         ret
 
-    _start:
     main:
 
+        mov eax, [esp+4] ;edi = argc
+        cmp eax, 2
+        jl .no_flag
 
+        mov eax, [esp+8]; edi = arg[0]
+        xor edx, edx
+        mov edx, [eax+4]
+        mov bl, byte [edx]
+        cmp bl, '-'
+        jz .pot_flag
+        jmp .no_flag
+
+        .pot_flag:
+            mov bl, byte [edx+1]
+            cmp bl, 'i'
+            jz .input
+            cmp bl, 'r'
+            jz .random
+            jmp .no_flag
+
+        .input:
+
+            mov eax, '1'
+            push eax
+            push seperator_foramt
+            call printf
+            add esp , 8
+
+            call get_multi
+            mov [in_struct], eax
+
+            mov eax, '2'
+            push eax
+            push seperator_foramt
+            call printf
+            add esp , 8
+
+            call get_multi
+            mov [in_struct_2], eax
+
+
+            mov eax, '1'
+            push eax
+            push multi_print_format
+            call printf
+            add esp ,8
+
+            mov eax, [in_struct]
+            push eax
+            call print_multi
+            add esp, 4
+
+            mov eax, '2'
+            push eax
+            push multi_print_format
+            call printf
+            add esp ,8
+
+            mov eax, [in_struct_2]
+            push eax
+            call print_multi
+            add esp, 4
+
+            mov ebx, 's'
+            push ebx
+            push multi_print_format
+            call printf
+            add esp ,8
+
+
+            mov eax, [in_struct]
+            push eax
+            mov eax, [in_struct_2]
+            push eax
+            call add_multi
+            add esp,8 
+
+            push eax
+            call print_multi
+            add esp, 4
+            jmp exit.finish
+
+        .random:
+            mov eax, '1'
+            push eax
+            push multi_print_format
+            call printf
+            add esp , 8
+
+            call PRmulti
+            mov [in_struct], eax
+            push eax
+            call print_multi
+            add esp,4
+
+
+            mov eax, '2'
+            push eax
+            push multi_print_format
+            call printf
+            add esp , 8
+
+            call PRmulti
+            mov [in_struct_2], eax
+            push eax
+            call print_multi
+            add esp,4
+
+            mov eax, 's'
+            push eax
+            push multi_print_format
+            call printf
+            add esp , 8
+
+            .stp:
+            mov eax, [in_struct]
+            push eax
+            mov eax, [in_struct_2]
+            push eax
+            call add_multi
+            add esp, 8
+
+            mov [in_struct], eax
+
+
+            mov eax, [in_struct]
+            push eax
+            call print_multi
+            add esp, 4
+            
+
+
+
+
+
+            jmp exit.finish
 
         
-        
-        push x_struct
-        push y_struct
-        call add_multi
-        add esp, 8
 
-        push eax
-        call print_multi
-        jmp exit.finish
+
+        .no_flag:
+            mov eax, 'x'
+            push eax
+            push multi_print_format
+            call printf
+            add esp ,8
+            push x_struct
+            call print_multi
+            add esp, 4
+
+            mov eax, 'y'
+            push eax
+            push multi_print_format
+            call printf
+            add esp ,8
+            push y_struct
+            call print_multi
+            add esp, 4
+
+        .addm:
+
+
+
+            mov ebx, 's'
+            push ebx
+            push multi_print_format
+            call printf
+            add esp ,8
+
+
+            mov eax, x_struct
+            push eax
+            mov eax, y_struct
+            push eax
+            call add_multi
+            add esp,8 
+
+            push eax
+            call print_multi
+            add esp, 4
+
+            mov eax, 0
+
+
+            jmp exit.finish
 
 
     
